43155fb2e12be8d87f6f52efbbe42641bbcf3f6d
Bohdan Tkachenko <git@dan.social>
2024-01-05 22:45:55 -0500

    Move dotfiles command from aliases to the bin dir

--- zsh/.zsh/.zshrc
+++ zsh/.zsh/.zshrc
@@ -33,4 +33,6 @@ antigen apply
 # To customize prompt, run `p10k configure` or edit ~/.zsh/.p10k.zsh.
 [[ ! -f ~/.zsh/.p10k.zsh ]] || source ~/.zsh/.p10k.zsh
 
-export EDITOR=vim
+export PATH="$PATH:$HOME/.bin"
+fpath=($HOME/.completion $fpath)
+
--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,17 +1,3 @@
 alias vialias='vim ~/.zsh/aliases.zsh && source ~/.zsh/aliases.zsh'
 alias vienv='vim ~/.zsh/env.zsh && source ~/.zsh/env.zsh'
 
-function dotfiles () {
-    case "$1" in
-        push)
-            (cd ~/.dotfiles && git add . && git commit && git push)
-        ;;
-        pull)
-            (cd ~/.dotfiles && git pull)
-        ;;
-        *)
-            echo "Usage $0 <push|pull>"
-        ;;
-    esac
-}
-
